33beec3bc7e817049be5b8cb9a88f77cef31cc12
Tharun1828 <91731118+Tharun1828@users.noreply.github.com>
2022-05-29 07:26:11 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/Tharun1828/Sk_Network-.git /Sk_Network-
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /Sk_Network-
 fi
 cd /EvaMaria
 pip3 install -U -r requirements.txt
